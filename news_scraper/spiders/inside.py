86eb16b973fe1cc911e5fe299a89f2f40b3eb07e
mikeku <mikekuforgit@gmail.com>
2021-01-02 12:24:04 +0800

    利用bs4模組開發網頁爬蟲

--- news_scraper/spiders/inside.py
+++ news_scraper/spiders/inside.py
@@ -1,10 +1,14 @@
 import scrapy
+import bs4
 
 
 class InsideSpider(scrapy.Spider):
     name = 'inside'
     allowed_domains = ['www.inside.com.tw']
-    start_urls = ['http://www.inside.com.tw/']
+    start_urls = ['https://www.inside.com.tw/tag/ai']
 
     def parse(self, response):
-        pass
+        soup = bs4.BeautifulSoup(response.text, 'lxml')
+        titles = soup.find_all('h3', {'class': 'post_title'})
+        for title in titles:
+            print(title.text.strip())
