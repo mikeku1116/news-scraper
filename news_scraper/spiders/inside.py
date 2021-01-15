41b136a57518db613f2e30f968414d8b9aa365da
mikeku <mikekuforgit@gmail.com>
2021-01-15 21:14:58 +0800

    scrapy css方法取得元素屬性值

--- news_scraper/spiders/inside.py
+++ news_scraper/spiders/inside.py
@@ -7,7 +7,7 @@ class InsideSpider(scrapy.Spider):
     start_urls = ['https://www.inside.com.tw/tag/ai']
 
     def parse(self, response):
-        titles = response.css("h3.post_title a::text").getall()
+        urls = response.css("a.js-auto_break_title::attr(href)").getall()
 
-        for title in titles:
-            print(title)
+        for url in urls:
+            print(url)
